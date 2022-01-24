7614259623fdd1c7b1a6457df214d777fd55bb1d
wcw <344078971@qq.com>
2022-01-24 10:10:29 +0800

    p13~16 refactor

---
+++ src/Shader.h
@@ -0,0 +1,33 @@
+#pragma once
+
+#include <string>
+#include <unordered_map>
+
+struct ShaderProgramSource
+{
+	std::string VertexSource;
+	std::string FragmentSource;
+};
+
+
+class Shader {
+private:
+	std::string m_FilePath;
+	unsigned int m_RendererID;
+	std::unordered_map<std::string, int> m_UniformLocationCache;
+public:
+	Shader(const std::string& filepath);
+	~Shader();
+
+	void Bind() const;
+	void Unbind() const;
+
+	void setUniform4f(const std::string& name, float f0, float f1, float f2, float f3);
+
+private:
+	unsigned int CompileShader(const std::string& source, unsigned int type);
+	unsigned int createShader(const std::string& vertexShader, const std::string& fragmentShader);
+	ShaderProgramSource ParseShader(const std::string& filepath);
+	unsigned int GetUniformLocation(const std::string& name);
+
+};
